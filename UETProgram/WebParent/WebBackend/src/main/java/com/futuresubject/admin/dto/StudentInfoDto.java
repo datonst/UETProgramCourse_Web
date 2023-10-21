542f1f0cdc5b028f1216a20962f0df806a7746e6
datonst <datonst@gmail.com>
2023-10-21 07:08:12 +0700

    add StudenInfoDto

---
+++ UETProgram/WebParent/WebBackend/src/main/java/com/futuresubject/admin/dto/StudentInfoDto.java
@@ -0,0 +1,39 @@
+package com.futuresubject.admin.dto;
+
+import com.futuresubject.common.entity.GenderType;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * DTO for {@link com.futuresubject.common.entity.Student}
+ */
+@Getter
+@AllArgsConstructor
+@Setter
+@Jacksonized
+@Builder
+@NoArgsConstructor
+public class StudentInfoDto implements Serializable {
+    String studentId;
+    String name;
+    Integer age;
+    String gender;
+    String address;
+    String phone;
+    String classFullName;
+    /**
+     * DTO Full Code for {@link com.futuresubject.common.entity.Program}
+     */
+    List<String> ProgramFullCode; // name of program which is student study
+    public void addProgramFullCode(String programFullCode) {
+        if (this.ProgramFullCode==null) {
+            this.ProgramFullCode = new ArrayList<>();
+        }
+        this.ProgramFullCode.add(programFullCode);
+    }
+}
