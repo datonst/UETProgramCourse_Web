a15b0d91b4cb82c9caaae3e40efcb6ca282bb4ef
datonst <datonst@gmail.com>
2024-04-15 21:22:39 +0700

    deploy cloud

--- UETProgram/WebParent/WebBackend/src/main/java/com/futuresubject/admin/config/security/WebConfig.java
+++ UETProgram/WebParent/WebBackend/src/main/java/com/futuresubject/admin/config/security/WebConfig.java
@@ -26,6 +26,7 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://stunet.site");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
